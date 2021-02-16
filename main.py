38273ff023926117a6093809729d2d9152c2dd9e
Lucas Wehmuth <lucas.wehmuth@gmail.com>
2021-02-16 19:56:28 -0300

    testing sublime extensions

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import assets
+import torch
+import pandas
 
 print("first commit - attempt 1")
 
 a = assets.Assets()
 print(a.name, a.quantity, a.price)
+
+boo = torch.nn()
+
+# aa = pandas.open_csv()
+
+x = 2*2+5
