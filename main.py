e92bc83e04d7157f086377498df77b3f94d18bb2
Lucas Wehmuth <lucas.wehmuth@gmail.com>
2021-02-16 16:01:19 -0300

    Commit #2 - New branch

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("first commit - attempt 1")
+import user
+
+# print("first commit - attempt 1")
+
+test_user = user.User()
+
+print("Name =", test_user.name, "| Age =", test_user.age)
---
+++ user.py
@@ -0,0 +1,4 @@
+class User():
+	def __init__(self):
+		self.name = "Lucas"
+		self.age = 26
