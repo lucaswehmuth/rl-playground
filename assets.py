b7e996771339e3b7d292cb8ddb41cf31959073a0
Lucas Wehmuth <lucas.wehmuth@gmail.com>
2021-02-16 16:27:58 -0300

    aaa

--- assets.py
+++
@@ -1,5 +0,0 @@
-class Assets():
-	def __init__(self):
-		self.name = "stock"
-		self.quantity = 100
-		self.price = 50
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import assets
 
 # print("first commit - attempt 1")
@@ -14,3 +15,6 @@ test_user = user.User()
 
 print("Name =", test_user.name, "| Age =", test_user.age)
 >>>>>>> e92bc83e04d7157f086377498df77b3f94d18bb2
+=======
+print("first commit - attempt 1")
+>>>>>>> parent of 42413d3... Test commit for conflicts
