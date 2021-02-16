42413d3b7307b4de9d2da8d6f0b91f728aa7e4d5
Lucas Wehmuth <lucas.wehmuth@gmail.com>
2021-02-16 16:06:36 -0300

    Test commit for conflicts

---
+++ assets.py
@@ -0,0 +1,5 @@
+class Assets():
+	def __init__(self):
+		self.name = "stock"
+		self.quantity = 100
+		self.price = 50
--- main.py
+++ main.py
@@ -1,1 +1,6 @@
+import assets
+
 print("first commit - attempt 1")
+
+a = assets.Assets()
+print(a.name, a.quantity, a.price)
